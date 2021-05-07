98e097294df2c59001649e38c56ffce50baaf75c
Rainer Koirikivi <rainer@koirikivi.fi>
2021-05-07 21:18:20 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+Bridge Rewarder
+===============
+
+A bot that gives RBTC to the Sovryn Citizens who move tokens to RSK via the token bridge
+
+Running:
+
+```
+python3.8 -m venv venv
+source ./venv/bin/activate
+pip install -e .
+sovryn_bridge_rewarder
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sovryn_bridge_rewarder',
+    version='0.0.1',
+    url='',
+    author='Sovryn',
+    packages=find_packages(exclude=['tests', 'tests*']),
+    package_data={'': [
+        'abi/*.json',
+    ]},
+    install_requires=[
+        'web3',
+        'sqlalchemy',
+    ],
+    extras_require={
+        'dev': [
+            # Testing
+            'pytest',
+
+            # Easier command-line experience
+            'ipython',
+            'ipdb',
+        ]
+    },
+    entry_points={
+        'console_scripts': [
+            'sovryn_bridge_rewarder=sovryn_bridge_rewarder.main:main',
+        ]
+    },
+)
